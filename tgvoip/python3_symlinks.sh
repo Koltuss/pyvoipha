7495c16a3d97faf3c7d088bfb0f62c18ed0a2b40
Koltuss <72206999+Koltuss@users.noreply.github.com>
2020-10-02 13:18:15 +0200

    Update python3_symlinks.sh

--- tgvoip/python3_symlinks.sh
+++ tgvoip/python3_symlinks.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd /usr/bin \
- && ln -sf easy_install-3.6 easy_install \
- && ln -sf idle3.6 idle \
- && ln -sf pydoc3.6 pydoc \
- && ln -sf python3.6 python \
- && ln -sf python-config3.6 python-config \
- && ln -sf pip3.6 pip \
+ && ln -sf easy_install-3.8 easy_install \
+ && ln -sf idle3.8 idle \
+ && ln -sf pydoc3.68 pydoc \
+ && ln -sf python3.8 python \
+ && ln -sf python-config3.8 python-config \
+ && ln -sf pip3.8 pip \
  && ln -sf /usr/include/locale.h /usr/include/xlocale.h
